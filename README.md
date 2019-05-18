52ce744d62b866977ef330e8c6d5dcafdc558af9
brenborbs <borbonbrennon@gmail.com>
2019-05-18 19:28:05 +0800

    Completed API authentication

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 - DB using MongoDB Atlas
 - Error validation using express-validator (validator/index.js)
 - Completed API development for Posts
+
+2nd commit:
+- Nodejs crypto documentation https://nodejs.org/api/crypto.html to hashed passwords
+- Virtual fields
+- Completed API development for Authentication(user signup and signin)
--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
 
 // load env variables
 const dotenv = require('dotenv');
-dotenv.config()
+dotenv.config();
 
 //db connection
 mongoose.connect(
@@ -22,13 +23,16 @@ mongoose.connection.on('error', err => {
 
 // routes
 const postRoutes = require('./routes/post');
+const authRoutes = require('./routes/auth');
 
 
 // middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(expressValidator());
 app.use("/", postRoutes);
+app.use("/", authRoutes);
 
 const port = 8080;
 app.listen(port, () => {
